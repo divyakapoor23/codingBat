034934e109927fb696bc31b2996fcf79f4fce102
Divya Kapoor <38455002+divyakapoor23@users.noreply.github.com>
2020-03-21 23:39:50 -0700

    Add files via upload

---
+++ MonkeySmile.java
@@ -0,0 +1,29 @@
+/* 
+* We have two monkeys, a and b, and the parameters 
+* aSmile and bSmile indicate if each is smiling. 
+* We are in trouble if they are both smiling or if neither of them is smiling. 
+* Return true if we are in trouble.
+
+
+* monkeyTrouble(true, true) → true
+* monkeyTrouble(false, false) → true
+* monkeyTrouble(true, false) → false
+*/
+
+public class MonkeySmile {
+   public static void main(String[] args) {
+      boolean aSmile = true;
+      boolean bSmile = true;
+      monkeyTrouble(aSmile, bSmile);
+   }
+
+   public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+      if(aSmile && bSmile) {
+         return true;
+      }
+      if(!aSmile && !bSmile) {
+         return true;
+      }
+  return false;
+  }
+}
---
+++ PositiveNegative.java
@@ -0,0 +1,28 @@
+/*
+* Given 2 int values, return true if one is negative and one is positive. 
+* Except if the parameter "negative" is true, then return true only 
+* if both are negative.
+
+* posNeg(1, -1, false) -> true
+* posNeg(-1, 1, false) -> true
+* posNeg(-4, -5, true) -> true
+*/
+public class PositiveNegative {
+   public static void main(String[] args) {
+      int a;
+      int b;
+      boolean negative;
+      posNeg(a, b, negative);
+   
+   }
+   
+   public static boolean posNeg(int a, int b, boolean negative) {
+      if(negative) {
+         return (a < 0 && b <0);
+       }
+       else {
+         return ((a < 0 && b > 0 ) || (a > 0 && b < 0));
+       }
+   
+   }
+}
