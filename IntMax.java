59cfa6ad38d22cb4eda6cc6c72cc8416e0707b67
Divya Kapoor <38455002+divyakapoor23@users.noreply.github.com>
2020-03-24 21:24:58 -0700

    Add files via upload

---
+++ IntMax.java
@@ -0,0 +1,27 @@
+/*
+* Given three int values, a b c, return the largest.
+* intMax(1, 2, 3) -> 3
+* intMax(1, 3, 2) -> 3
+* intMax(3, 2, 1) -> 3
+*/
+public class IntMax {
+   public static void main(String[] args) {
+      int a;
+      int b;
+      int c;
+      intMax(1, 2, 3);
+   }
+   public static int intMax(int a, int b, int c) {
+      int max;
+      if(a > b) {
+         max = a;
+      }
+      else {
+         max = b;
+      }
+      if(c > max) {
+         max = c;
+      }
+      return max;
+   }
+}
